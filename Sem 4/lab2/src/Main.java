21f13f3864728ffef0d2b56d0dfb3f5fb05766bd
Perfomo <31494.perfomo@gmail.com>
2023-03-12 22:30:20 +0300

    Ready

--- Sem 4/lab2/src/Main.java
+++ Sem 4/lab2/src/Main.java
@@ -30,10 +30,76 @@ public class Main {
         b1.change_amount();
         b1.get_info("Linux...");
     }
+    public static void task_5() {
+        Double d = Double.valueOf("3.2");
+        String l = "3.33";
+        double s = Double.parseDouble(l);
+        int k = d.intValue();
+        long t = d.longValue();
+        short r = d.shortValue();
+        float q = d.floatValue();
+        byte z = d.byteValue();
+        System.out.println("\nDouble: " + d + "\ndouble: " + d + "\nint: " + k + "\nlong: " + t + "\nshort: " + r + "\nfloat: " + q + "\nbyte: " + z);
+        String aa = Double.toString(3.14);
+        System.out.println("\n" + aa);
+    }
     public static void task_4() {
+        Fraction2 f1 = new Fraction2(3, 5);
+        Fraction2 f2 = new Fraction2(9, 3);
+        Fraction2 f3 = new Fraction2(-1, 4);
+        System.out.println("\n++++++++++");
+        f1.sum(f2, f3);
+        f1.show();
+        f1.reset(3, 5);
+        System.out.println("\n----------");
+        f1.min(f2);
+        f1.show();
+        f1.reset(3, 5);
+        f1.min(f3);
+        f1.show();
+        f1.reset(3, 5);
+        System.out.println("\n**********");
+        f1.mult(f2, f3);
+        f1.show();
+        f1.reset(3, 5);
+        System.out.println("\n//////////");
+        f1.div(f2);
+        f1.show();
+        f1.reset(3, 5);
+        f1.div(f3);
+        f1.show();
+        f1.reset(3, 5);
+        System.out.println("\n{}{}{}{}{}{}");
+        int k = (int) (Math.random() * (11 - 3) + 3);
+        Fraction2[] arr = new Fraction2[k];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = new Fraction2();
+            arr[i].cut();
+            System.out.print(arr[i].m + "/" + arr[i].n + "  ");
+        }
+        System.out.println();
+        Fraction2.idk(arr);
+    }
+    public static void task_6() {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("\nInput string: ");
+        str = sc.nextLine();
+        System.out.println("Str: " + str);
+        System.out.println("Last char is: " + str.charAt(str.length()-1));
+        System.out.println("Str ends with '!!!' - " + (str.endsWith("!!!") ? "True" : "False"));
+        System.out.println("Str starts with 'I like' - " + (str.startsWith("I like") ? "True" : "False"));
+        System.out.println("Str contains 'Java' - " + (str.contains("Java") ? "True" : "False"));
+        System.out.println("Str index of 'Java' - " + str.indexOf("Java"));
+        System.out.println("Str with 'o' instead of 'a' - " + str.replaceAll("a", "o"));
+        System.out.println("Str in upper case - " + str.toUpperCase());
+        System.out.println("Str in lower case - " + str.toLowerCase());
+        System.out.println("Str's three first letters - " + str.substring(0,3));
+    }
+    public static void task_7() {
         Fraction f1 = new Fraction(3, 5);
         Fraction f2 = new Fraction(9, 3);
-        Fraction f3 = new Fraction(1, 4);
+        Fraction f3 = new Fraction(-1, 4);
         System.out.println("\n++++++++++");
         f1.sum(f2, f3);
         f1.reset(3, 5);
@@ -60,41 +126,12 @@ public class Main {
         System.out.println();
         arr = Fraction.idk(arr);
     }
-    public static void task_5() {
-        Double d = Double.valueOf("3.2");
-        String l = "3.33";
-        double s = Double.parseDouble(l);
-        int k = d.intValue();
-        long t = d.longValue();
-        short r = d.shortValue();
-        float q = d.floatValue();
-        byte z = d.byteValue();
-        System.out.println("\nDouble: " + d + "\ndouble: " + d + "\nint: " + k + "\nlong: " + t + "\nshort: " + r + "\nfloat: " + q + "\nbyte: " + z);
-        String aa = Double.toString(3.14);
-        System.out.println("\n" + aa);
-    }
-    public static void task_6() {
-        Scanner sc = new Scanner(System.in);
-        String str;
-        System.out.println("\nInput string: ");
-        str = sc.nextLine();
-        System.out.println("Str: " + str);
-        System.out.println("Last char is: " + str.charAt(str.length()-1));
-        System.out.println("Str ends with '!!!' - " + (str.endsWith("!!!") ? "True" : "False"));
-        System.out.println("Str starts with 'I like' - " + (str.startsWith("I like") ? "True" : "False"));
-        System.out.println("Str contains 'Java' - " + (str.contains("Java") ? "True" : "False"));
-        System.out.println("Str index of 'Java' - " + str.indexOf("Java"));
-        System.out.println("Str with 'o' instead of 'a' - " + str.replaceAll("a", "o"));
-        System.out.println("Str in upper case - " + str.toUpperCase());
-        System.out.println("Str in lower case - " + str.toLowerCase());
-        System.out.println("Str's three first letters - " + str.substring(0,3));
-    }
 
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     boolean exit = false;
     while (!exit) {
-        System.out.println("\nInput task number [1, 14]\n0 - exit");
+        System.out.println("\nInput task number [1, 7]\n0 - exit");
         if (sc.hasNextInt()) {
             int i = sc.nextInt();
             switch (i) {
@@ -108,6 +145,7 @@ public class Main {
                 case 4 -> task_4();
                 case 5 -> task_5();
                 case 6 -> task_6();
+                case 7 -> task_7();
                 default -> System.out.println("\nError...");
             }
         }
